ab837986dde66ebe933504a2f650e9c0dfa6d582
Victor <afterisbest@gmail.com>
2023-09-09 03:03:30 -0300

    up all project

---
+++ app/layout.tsx
@@ -0,0 +1,46 @@
+import Sidebar from '@/components/Sidebar'
+import './globals.css'
+import type { Metadata } from 'next'
+import { Figtree } from 'next/font/google'
+import SupabaseProvider from '@/providers/Supabase'
+import UserProvider from '@/providers/userProvider'
+import ModalProvider from '@/providers/ModalProvider'
+import ToasterProvider from '@/providers/ToasterProvider'
+import getSongsByUserId from '@/actions/getSongsByUserId'
+import Player from '@/components/Player'
+import getActiveProductsWithPrices from '@/actions/getActiveProductsWithPrice'
+
+const font = Figtree({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Spotify Clone',
+  description: 'Listen to music',
+}
+
+export const revalidate = 0
+
+export default async function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const userSongs = await getSongsByUserId()
+  const products = await getActiveProductsWithPrices()
+
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <ToasterProvider />
+        <SupabaseProvider>
+          <UserProvider>
+            <ModalProvider  products={products}/>
+            <Sidebar songs={userSongs}>
+              {children}
+            </Sidebar>
+            <Player />
+          </UserProvider>
+        </SupabaseProvider>
+      </body>
+    </html>
+  )
+}
---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains:[
+            "zsxnwmdmivrlxsegxclk.supabase.co"
+        ]
+    }
+}
+
+module.exports = nextConfig
